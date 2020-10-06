c253c801c3dd3d6229307c6f325e6f896a4beb4b
xScoopy <JLeary070@gmail.com>
2020-10-06 16:32:48 -0700

    finished stretch challenges

--- scripts.js
+++ scripts.js
@@ -7,21 +7,30 @@ const fontType = document.querySelector('#select-font')
 const fontColor = document.querySelector('#input-color')
 const bgColor = document.querySelector('#input-bg-color')
 const inputText = document.querySelector('#enter-text')
-
+const fontWeight = document.querySelector('#input-weight')
+const fontStyle = document.querySelector('#input-style')
+const fontSpacing = document.querySelector('#input-spacing')
+const fontPadding = document.querySelector('#input-padding')
 //display elements
 const sampleText = document.querySelector('#display')
 const showSize = document.querySelector('#show-size')
 const showFont = document.querySelector('#show-font')
 const showColor = document.querySelector('#show-color')
 const showBgColor = document.querySelector('#show-bg-color')
-
+const showWeight = document.querySelector('#show-weight')
+const showStyle = document.querySelector('#show-style')
+const showSpacing = document.querySelector('#show-spacing')
+const showPadding = document.querySelector('#show-padding')
 //event listeners
 fontSize.addEventListener('input', handleInput)
 fontType.addEventListener('input', handleInput)
 fontColor.addEventListener('input', handleInput)
 bgColor.addEventListener('input', handleInput)
 inputText.addEventListener('input', handleInput)
-
+fontWeight.addEventListener('input', handleInput)
+fontStyle.addEventListener('input', handleInput)
+fontSpacing.addEventListener('input', handleInput)
+fontPadding.addEventListener('input', handleInput)
 //input handler function
 function handleInput() {
     //font size
@@ -43,5 +52,21 @@ function handleInput() {
     //display text
     const newText = inputText.value
     sampleText.innerHTML = newText
+    //font weight
+    const newWeight = fontWeight.value
+    display.style.fontWeight = newWeight
+    showWeight.innerHTML = newWeight
+    //font style
+    const newStyle = fontStyle.value
+    display.style.fontStyle = newStyle
+    showStyle.innerHTML = newStyle
+    //letter spacing
+    const newSpacing = fontSpacing.value + 'px'
+    display.style.letterSpacing = newSpacing
+    showSpacing.innerHTML = newSpacing
+    //padding
+    const newPadding = fontPadding.value + 'px'
+    display.style.padding = newPadding
+    showPadding.innerHTML = newPadding
 
 }
